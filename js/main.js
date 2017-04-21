99170fb55ced10b5939a64633aa60f85991f3f7a
Anish.Krishnaswamy <kanish671@yahoo.com>
2017-04-22 00:13:14 +0530

    adding details about work ex

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
 	var contactButton = document.getElementById('contact-link');
 	contactButton.addEventListener('click', function() {
-		window.location.href = 'mailto:kanish671@yahoo.com';
+		window.location.href = 'mailto:kanish671@gmail.com';
 	}, false);
 })();
