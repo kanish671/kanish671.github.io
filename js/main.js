e05598f9470fab975b294b8e63959f21c093c1e6
Anish.Krishnaswamy <kanish671@yahoo.com>
2017-04-21 23:30:25 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+(function() {
+	var contactButton = document.getElementById('contact-link');
+	contactButton.addEventListener('click', function() {
+		window.location.href = 'mailto:kanish671@yahoo.com';
+	}, false);
+})();
