0cccc7cdc888b5956d9cf8b3552259a3c7c8ca26
anishk <anish.krishnaswamy@target.com>
2020-09-28 02:25:53 +0530

    updating website with latest information and links

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@
 	contactButton.addEventListener('click', function() {
 		window.location.href = 'mailto:kanish671@gmail.com';
 	}, false);
+	$('.ui.sidebar').sidebar({
+		context: $('body'),
+		transition: 'overlay'
+	}).sidebar('attach events', '#mobile-menu');
 })();
